5ecdf1365d3aac716ec5f7051176b918a283e7aa
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-08-29 13:30:23 -0300

    Create 4-spacejam.js

---
+++ challenges/4-spacejam.js
@@ -0,0 +1,17 @@
+/*
+S P A C E J A M
+Given a string, remove all spaces from the string, insert two spaces between every character, convert all alphabetical letters to uppercase, and return the result.
+
+Non-alphabetical characters should remain unchanged (except for spaces).
+
+Tests:
+Passed:1. spaceJam("freeCodeCamp") should return "F  R  E  E  C  O  D  E  C  A  M  P".
+Passed:2. spaceJam("   free   Code   Camp   ") should return "F  R  E  E  C  O  D  E  C  A  M  P".
+Passed:3. spaceJam("Hello World?!") should return "H  E  L  L  O  W  O  R  L  D  ?  !".
+Passed:4. spaceJam("C@t$ & D0g$") should return "C  @  T  $  &  D  0  G  $".
+Passed:5. spaceJam("allyourbase") should return "A  L  L  Y  O  U  R  B  A  S  E".
+*/
+
+function spaceJam(s) {
+  return Array.from(s.toUpperCase().replaceAll(" ", "")).join("  ");
+}
