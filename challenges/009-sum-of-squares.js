b423ca83842d79f8d7866852e3193d9901228e2b
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-09-02 14:19:01 -0300

    Create 009-sum-of-squares.js

---
+++ challenges/009-sum-of-squares.js
@@ -0,0 +1,18 @@
+/*
+Sum of Squares
+Given a positive integer up to 1,000, return the sum of all the integers squared from 1 up to the number.
+
+Tests:
+Passed:1. sumOfSquares(5) should return 55.
+Passed:2. sumOfSquares(10) should return 385.
+Passed:3. sumOfSquares(25) should return 5525.
+Passed:4. sumOfSquares(500) should return 41791750.
+Passed:5. sumOfSquares(1000) should return 333833500.
+*/
+
+function sumOfSquares(n) {
+  if (n > 1000) n = 1000;
+  let sum = 0;
+  for (let i = 1; i <= n; i++) sum += i ** 2;
+  return sum;
+}
