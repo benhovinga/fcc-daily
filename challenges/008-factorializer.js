dd910926fc337f1ea16d1c24be15d266d1be652f
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-09-02 13:28:33 -0300

    Create 008-factorializer.js

---
+++ challenges/008-factorializer.js
@@ -0,0 +1,18 @@
+/*
+Factorializer
+Given an integer from zero to 20, return the factorial of that number. The factorial of a number is the product of all the numbers between 1 and the given number.
+
+The factorial of zero is 1.
+
+Tests:
+Passed:1. factorial(0) should return 1.
+Passed:2. factorial(5) should return 120.
+Passed:3. factorial(20) should return 2432902008176640000.
+*/
+
+function factorial(n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = 2; i <= n; i++) product *= i;
+  return product;
+}
