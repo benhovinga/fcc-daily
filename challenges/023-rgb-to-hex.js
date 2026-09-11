eeeffc6a46e7018f756240163b936457afd07937
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-09-11 10:37:52 -0300

    Create 023-rgb-to-hex.js

---
+++ challenges/023-rgb-to-hex.js
@@ -0,0 +1,26 @@
+/*
+RGB to Hex
+Given a CSS rgb(r, g, b) color string, return its hexadecimal equivalent.
+
+Here are some example outputs for a given input:
+
+Input	Output
+"rgb(255, 255, 255)"	"#ffffff"
+"rgb(1, 2, 3)"	"#010203"
+Make any letters lowercase.
+Return a # followed by six characters. Don't use any shorthand values.
+
+Tests:
+Passed:1. rgbToHex("rgb(255, 255, 255)") should return "#ffffff".
+Passed:2. rgbToHex("rgb(1, 11, 111)") should return "#010b6f".
+Passed:3. rgbToHex("rgb(173, 216, 230)") should return "#add8e6".
+Passed:4. rgbToHex("rgb(79, 123, 201)") should return "#4f7bc9".
+*/
+
+function rgbToHex(rgb) {
+  const re = /rgb\((\d{1,3})\D*(\d{1,3})\D*(\d{1,3})\)/i;
+  const [r, g, b] = [...rgb.matchAll(re)][0]
+    .filter((_, i) => i != 0)
+    .map(c => Number.parseInt(c).toString(16).padStart(2, "0"));
+  return `#${r}${g}${b}`;
+}
