60e8c1f79025615142eb48dc2766f9f258ac8ec3
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2026-09-14 13:22:02 -0300

    Create 020-array-duplicates.js

---
+++ challenges/020-array-duplicates.js
@@ -0,0 +1,21 @@
+/*
+Array Duplicates
+Given an array of integers, return an array of integers that appear more than once in the initial array, sorted in ascending order. If no values appear more than once, return an empty array.
+
+Only include one instance of each value in the returned array.
+Tests:
+Passed:1. findDuplicates([1, 2, 3, 4, 5]) should return [].
+Passed:2. findDuplicates([1, 2, 3, 4, 1, 2]) should return [1, 2].
+Passed:3. findDuplicates([2, 34, 0, 1, -6, 23, 5, 3, 2, 5, 67, -6, 23, 2, 43, 2, 12, 0, 2, 4, 4]) should return [-6, 0, 2, 4, 5, 23].
+*/
+
+function findDuplicates(arr) {
+  const seen = new Set();
+  const duplicates = new Set();
+  arr.forEach(num => {
+    if (seen.has(num))
+      duplicates.add(num);
+    seen.add(num);
+  });
+  return [...duplicates].sort((a, b) => a - b);
+}
